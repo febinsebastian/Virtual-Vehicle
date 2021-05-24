3b44ae7c2f5afc2154d2f4a66e0f146626792390
Febin Sebastian <febines1@gmail.com>
2021-05-24 19:12:38 +0200

    Client Initial Code

--- Server/app.js
+++ Server/app.js
@@ -15,7 +15,7 @@ app.use((error, req, res, next) =>{
     res.json({message: error.message || 'An unexpected error occurred!'})
 });
 
-mongoose.connect('mongodb+srv://febin:A7RFf15lwmRzl2jK@cluster0.zsag9.mongodb.net/vehicle?retryWrites=true&w=majority').then(() => {
+mongoose.connect('mongodb+srv://<usename>:<password>@cluster0.zsag9.mongodb.net/vehicle?retryWrites=true&w=majority').then(() => {
     app.listen(8000, ()=> {
         console.log('listening on port 8000');
       })
